f5045f96fc4567d662593ffae43905766051ee71
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2023-04-01 15:40:15 +0300

    updates

---
+++ guess_a_word.py
@@ -0,0 +1,25 @@
+import random
+
+WORDS = ['apple', 'banana', 'cherry', 'dragonfruit', 'elderberry', 'fig', 'grapefruit', 'honeydew', 'imbe', 'jackfruit']
+
+MAX_TRIES = 7
+
+print("Welcome to the word guessing game!")
+print(f"I'm thinking of a fruit name. Can you guess it in {MAX_TRIES} tries?")
+
+secret_word = random.choice(WORDS)
+
+for guess_count in range(1, MAX_TRIES + 1):
+    guess = input(f"Guess #{guess_count}: ")
+
+    if not guess.isalpha():
+        print("Invalid input. Please enter a single word.")
+        continue
+
+    if guess.lower() == secret_word:
+        print(f"Congratulations! You guessed the word '{secret_word}' in {guess_count} tries!")
+        break
+    else:
+        print("Wrong! Guess again.")
+else:
+    print(f"Sorry, you didn't guess the word. It was '{secret_word}'. Better luck next time!")
